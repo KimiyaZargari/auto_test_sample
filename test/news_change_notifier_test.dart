85ccfe1388628fd8b810f77879704ef4898a2a2b
kimiya zargari <kimiyazargari13@gmail.com>
2022-10-26 21:15:20 +0330

    implement get articles with tests

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -20,4 +20,31 @@ void main() {
     expect(sut.articles, []);
     expect(sut.isLoading, false);
   });
+  group('getArticles', () {
+    final articlesUnEmptyList = [
+      Article(title: 'test1', content: 'test1 content'),
+      Article(title: 'test2', content: 'test2 content'),
+      Article(title: 'test3', content: 'test3 content'),
+    ];
+    void arrangeNewsServiceReturnsUnEmptyListOfArticles() {
+      when(() => mockNewsService.getArticles())
+          .thenAnswer((invocation) async => articlesUnEmptyList);
+    }
+
+    test('gets articles', () async {
+      arrangeNewsServiceReturnsUnEmptyListOfArticles();
+      await sut.getArticles();
+      verify(() => mockNewsService.getArticles()).called(1);
+    });
+    test(
+        'indicates loading of data, setting articles to the data loaded, and finished loading',
+        () async {
+      arrangeNewsServiceReturnsUnEmptyListOfArticles();
+      final future = sut.getArticles();
+      expect(sut.isLoading, true);
+      await future;
+      expect(sut.articles, articlesUnEmptyList);
+      expect(sut.isLoading, false);
+    });
+  });
 }
